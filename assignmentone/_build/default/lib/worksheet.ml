11f23128f52ead6f36cf9f723b71bb8ec8556881
adrianfrancalanza <adrian.francalanza@um.edu.mt>
2025-10-03 16:32:25 +0200

    the assignment files

---
+++ assignmentone/_build/default/lib/worksheet.ml
@@ -0,0 +1,32 @@
+(* Question 1 *)
+
+let is_even (_x : int): bool =
+(* ---------------- edit here begin ---------------- *)
+  failwith "TODO" (* dummy code *)
+(* ----------------- edit here end ----------------- *)
+
+let remove_int (_l: int list) (_n: int) =
+(* ---------------- edit here begin ---------------- *)
+  failwith "TODO" 
+(* ----------------- edit here end ----------------- *)
+
+let sum (_l : int list): int =
+(* ---------------- edit here begin ---------------- *)
+  failwith "TODO" 
+(* ----------------- edit here end ----------------- *)  
+
+let rev (_l : 'a list) : 'a list =
+(* ---------------- edit here begin ---------------- *)
+  failwith "TODO" 
+(* ----------------- edit here end ----------------- *)
+
+let even_sized_palindrome (_l: 'a list): 'a list  = 
+(* ---------------- edit here begin ---------------- *)
+  failwith "TODO" 
+(* ----------------- edit here end ----------------- *)
+
+
+let is_palindrome (_l: 'a list): bool = 
+(* ---------------- edit here begin ---------------- *)
+  failwith "TODO" 
+(* ----------------- edit here end ----------------- *)
